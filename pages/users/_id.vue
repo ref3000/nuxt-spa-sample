e28e8b48f2b5ba9f953287fa9cbf643add9a54a8
yamamoto-kiyohiro <yamamoto_kiyohiro@cyberagent.co.jp>
2019-05-27 14:45:16 +0900

    dynamic routing on SPA mode!!!

---
+++ pages/users/_id.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="container">
+    <div>
+      {{ $route.params }}
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  validate ({ params }) {
+    // 数値でなければならない
+    return /^\d+$/.test(params.id)
+  }
+}
+</script>
+
+<style>
+</style>
